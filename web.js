a3b0bae486944d7af2a64c4627a7bafabb0610e0
Jaizen <jecklyde@gmail.com>
2013-07-12 05:56:49 +0000

    update message

--- web.js
+++ web.js
@@ -1,15 +1,12 @@
-#!/usr/bin/env node
-var express = require('express');
 var fs = require('fs');
+var express = require('express');
+
 var app = express.createServer(express.logger());
-var msg = fs.readFileSync("index.html");
-var buf = new Buffer(msg);
 
 app.get('/', function(request, response) {
-  response.send(buf.toString());
+  var content = fs.readFileSync('index.html');
+  response.send(content.toString());
 });
-
-console.log(buf.toString());
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
